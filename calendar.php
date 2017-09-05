29067f74e4625eee1891139a5d737f3a6ae304d1
Jimako <janamicek@gmail.com>
2017-09-05 14:01:35 +0200

    updated calendar page

--- calendar.php
+++ calendar.php
@@ -77,24 +77,21 @@ $ecal_class = new ecal_class;
 
 if(deftrue('BOOTSTRAP') === 3)  {
    $calendartemplate   = e107::getTemplate('calendar_menu', 'calendarbootstrap3' );
+   $calSc->wrapper('calendarbootstrap3/calendar');
 }
 else {
 	 $calendartemplate   = e107::getTemplate('calendar_menu', 'calendarlegacy' );
+	 $calSc->wrapper('calendarlegacy/calendar');
 } 
-  
+
+$CALENDAR_TIME_TABLE_START      = $calendartemplate['calendar']['time_table_start'];  
 $CALENDAR_TIME_TABLE		        = $calendartemplate['calendar']['time_table']; 
-$CALENDAR_TIME_TABLE_END        = $calendartemplate['calendar']['time_table_end'];
-$CALENDAR_NAVIGATION_TABLE      = $calendartemplate['calendar']['calendar_navigation_table'];	
-
-
+$CALENDAR_NAVIGATION_TABLE      = $calendartemplate['calendar']['navigation_table'];	
 $CALENDAR_CALENDAR_HEADER_START = $calendartemplate['calendar']['calendar_header_start'];
 $CALENDAR_CALENDAR_HEADER       = $calendartemplate['calendar']['calendar_header'];
 $CALENDAR_CALENDAR_HEADER_END   = $calendartemplate['calendar']['calendar_header_end'];
-
 $CALENDAR_CALENDAR_START        = $calendartemplate['calendar']['calendar_start'];
 $CALENDAR_CALENDAR_END          = $calendartemplate['calendar']['calendar_end'];
-
-
 $CALENDAR_CALENDAR_WEEKSWITCH   = $calendartemplate['calendar']['calendar_weekswitch'];
 $CALENDAR_CALENDAR_DAY_NON      = $calendartemplate['calendar']['calendar_day_non'];
 $CALENDAR_CALENDAR_DAY_TODAY    = $calendartemplate['calendar']['calendar_day_today'];
@@ -103,6 +100,7 @@ $CALENDAR_SHOWEVENT             = $calendartemplate['calendar']['showevent'];
 $CALENDAR_CALENDAR_DAY_EMPTY    = $calendartemplate['calendar']['calendar_day_empty'];
 $CALENDAR_CALENDAR_DAY_END      = $calendartemplate['calendar']['calendar_day_end'];
 
+$CALENDAR_TIME_TABLE_END        = $calendartemplate['calendar']['time_table_end'];
 $cat_filter = intval(varset($_POST['event_cat_ids'],-1));
 if ($cat_filter == -1) $cat_filter = '*';
 
@@ -144,8 +142,9 @@ $calSc->catFilter = $cat_filter;
 //-------------------------------------------------
 
 // time switch buttons
-$cal_text 		= $tp->parseTemplate($CALENDAR_TIME_TABLE, FALSE, $calSc);
-$cal_text_end = $tp->parseTemplate($CALENDAR_TIME_TABLE_END, FALSE, $calSc);
+$cal_text_start	= $tp->parseTemplate($CALENDAR_TIME_TABLE_START, FALSE, $calSc);
+$cal_text 		  = $tp->parseTemplate($CALENDAR_TIME_TABLE, FALSE, $calSc);
+$cal_text_end   = $tp->parseTemplate($CALENDAR_TIME_TABLE_END, FALSE, $calSc);
 
 // navigation buttons
 $nav_text = $tp->parseTemplate($CALENDAR_NAVIGATION_TABLE, FALSE, $calSc);
@@ -305,7 +304,7 @@ if($loop!=0)
 }
 $text .= $tp->parseTemplate($CALENDAR_CALENDAR_END, FALSE, $calSc);
 
-$ns->tablerender(EC_LAN_79, $cal_text . $nav_text . $text . $cal_text_end);
+$ns->tablerender(EC_LAN_79, $cal_text_start.$cal_text . $nav_text . $text . $cal_text_end);
 
 // Claim back memory from key variables
 unset($ev_list);
