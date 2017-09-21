f6af776951f9923343eedcc1c96371f3f58bf26c
Jimako <janamicek@gmail.com>
2017-09-21 16:20:52 +0200

    preparing for new admin

--- calendar_sql.php
+++ calendar_sql.php
@@ -16,7 +16,7 @@ CREATE TABLE event (
 	event_rec_y tinyint(2) unsigned NOT NULL default '0',
 	PRIMARY KEY  (event_id),
 	KEY event_start (event_start)
-	) ENGINE=MyISAM;,
+	) ENGINE=MyISAM;
 	CREATE TABLE event_cat (
 	event_cat_id smallint(5) unsigned NOT NULL auto_increment,
 	event_cat_name varchar(100) NOT NULL default '',
@@ -34,7 +34,7 @@ CREATE TABLE event (
 	event_cat_description text,
 	event_cat_force_class int(10) unsigned NOT NULL default '0',
 	PRIMARY KEY  (event_cat_id)
-	) ENGINE=MyISAM;,
+	) ENGINE=MyISAM;
 	CREATE TABLE event_subs (
 	event_subid int(10) unsigned NOT NULL auto_increment,
 	event_userid  int(10) unsigned NOT NULL default '0',
